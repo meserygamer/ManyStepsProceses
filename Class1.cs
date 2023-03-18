12719790271b595ca1a0a498915aa49ee2bf9245
Meserygg <Meserygamer@gmail.com>
2023-03-18 09:30:25 +0300

    Ver0.1

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyStepsProceses
+{
+    internal class TreeNode
+    {
+        struct Naslednik //Структура для описания наследников узла
+        {
+            TreeNode NextNode; //Следующая вершина
+            int NextNodePath; //Путь до следующей вершины
+            int? WNextNodePath = null; //Путь до следующей вершины учитывая последующие маршруты
+            public Naslednik(TreeNode NextNode, int NextNodePath)
+            {
+                this.NextNode = NextNode;
+                this.NextNodePath = NextNodePath;
+            }
+        }
+        public int ID_Node { get; set; } //Номер вершины
+        List<TreeNode> Parents = new List<TreeNode>(); //Отцовские элементы
+        List<Naslednik> Nasledniki = new List<Naslednik>(); //наследники
+        Naslednik? U; //Оптимальный наследник
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите количество вершин графа");
+            int k = Convert.ToInt32(Console.ReadLine());
+            int[,] MasVersh = new int[k, k];
+            for(int i = 0; i < k; i++)
+            {
+                int j = 0;
+                foreach (var l in Array.ConvertAll(Console.ReadLine().Split(" "), str => Convert.ToInt32(str)))
+                {
+                    MasVersh[i, j++] = l;
+                }
+            }
+            ZadachaManyStepsProceses Zad = new ZadachaManyStepsProceses(MasVersh);
+        }
+        class ZadachaManyStepsProceses
+        {
+            TreeNode[] Nodes;
+            public ZadachaManyStepsProceses(int[,] MasVersh /*Массив отношений вершин*/)
+            {
+                Nodes = new TreeNode[MasVersh.GetLength(0)]; //Создаём массив на введённое количество вершин
+                
+            }
         }
     }
 }

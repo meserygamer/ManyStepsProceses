26c1f537ff43ec79c50431511ab8f779e06764c4
meserygamer <meserygamer@gmail.com>
2023-03-19 21:52:01 +0300

    ver 0.2

--- Class1.cs
+++ Class1.cs
@@ -1,27 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ManyStepsProceses
 {
-    internal class TreeNode
+    internal class TreeGraph
     {
-        struct Naslednik //Структура для описания наследников узла
+        TreeNode[] TreeNodes; //Узлы графа
+        int[,] MatrixPaths; //Матрица путей
+        public TreeGraph(int[,] MatrixPaths) //Получает матрицу на вход и формирует по ней узлы
         {
-            TreeNode NextNode; //Следующая вершина
-            int NextNodePath; //Путь до следующей вершины
-            int? WNextNodePath = null; //Путь до следующей вершины учитывая последующие маршруты
-            public Naslednik(TreeNode NextNode, int NextNodePath)
+            this.MatrixPaths = MatrixPaths;
+            TreeNodes = new TreeNode[MatrixPaths.GetLength(0)];
+        }
+        private void FormGraph() //Создаёт узлы графа
+        {
+            for(int i = 0; i < MatrixPaths.GetLength(0); i++)
             {
-                this.NextNode = NextNode;
-                this.NextNodePath = NextNodePath;
+                TreeNodes[i] = new TreeNode(i+1);
+            }
+            for (int i = 0; i < MatrixPaths.GetLength(0); i++)
+            {
+                TreeNodes[i].FormRelation(TreeNodes, MatrixPaths);
             }
         }
-        public int ID_Node { get; set; } //Номер вершины
-        List<TreeNode> Parents = new List<TreeNode>(); //Отцовские элементы
-        List<Naslednik> Nasledniki = new List<Naslednik>(); //наследники
-        Naslednik? U; //Оптимальный наследник
+    }
+    internal class TreeNode
+    {
+        struct Relation //Структура для описания отношений между узлами
+        {
+            TreeNode Node; //Узел для создания связи
+            int NodePath; //Путь до узла
+            //int? WNextNodePath = null; //Путь до следующей вершины учитывая последующие маршруты
+            public Relation(TreeNode NextNode, int NextNodePath)
+            {
+                this.Node = NextNode;
+                this.NodePath = NextNodePath;
+            }
+        }
+        public int ID_Node {get; protected set;} //Номер вершины
+        List<Relation> Parents = new List<Relation>(); //Отцовские элементы
+        List<Relation> Nasledniki = new List<Relation>(); //наследники
+        public TreeNode(int ID_Node)
+        {
+            this.ID_Node = ID_Node;
+        }
+        public void FormRelation(TreeNode[] Graph, int[,] MatrixPath) //Функция формирует связи узла с другими узлами из вектора Graph, используя матрицу путей MatrixPath
+        {
+            for(int j = 0; j < MatrixPath.GetLength(1); j++) if(MatrixPath[(ID_Node-1), j] > 0)
+            {
+                Nasledniki.Add(new Relation(Graph[j], MatrixPath[(ID_Node - 1), j]));
+                Graph[j].Parents.Add(new Relation(this, MatrixPath[(ID_Node - 1), j]));
+            }
+        }
+        //Naslednik? U; //Оптимальный наследник
     }
 }
